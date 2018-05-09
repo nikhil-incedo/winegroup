95a5932b0cc71a90285c213aa940b47cf61d1861
nik <nikhil-incedo@incedoinc.com>
2018-05-09 19:01:02 +0530

    Change headers

--- participant/list.php
+++ participant/list.php
@@ -2,6 +2,7 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../config/database.php';
 include_once '../models/participant.php';
